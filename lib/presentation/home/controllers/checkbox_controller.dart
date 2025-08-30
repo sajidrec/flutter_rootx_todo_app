9a2330d54d93d3d00277b4e98cb95cdf7d70765b
MD. Sajid Hossain <sajidrec@gmail.com>
2025-08-30 17:40:08 +0600

    Integrate GetX for checkbox state management
    This commit introduces GetX for managing the state of checkboxes in the task list.
    
    Key changes:
    - Added `CheckboxController` to handle checkbox state.
    - Integrated `CheckboxController` into `HomePage` for task checkboxes.
    - Updated `_buildTask` to use `GetBuilder` for checkbox and task title, enabling dynamic updates based on checkbox state (e.g., strikethrough text when checked).
    - Added an "eye" icon to view the full task description in a dialog.
    - Registered `CheckboxController` in `ControllerBinders`.
    - Adjusted FAB icon size.

--- lib/presentation/home/controllers/checkbox_controller.dart
+++ lib/presentation/home/controllers/checkbox_controller.dart
@@ -1,4 +1,5 @@
-import 'package:get/get.dart';
+
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class CheckboxController extends GetxController {
   bool isChecked = false;
